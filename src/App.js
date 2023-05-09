2a91ee862b19d34ac5bbfa6b6d9257e3e1348a8b
erickcampaz <erickcampaz@MacBook-Pro-de-erickcampaz.local>
2023-05-08 20:12:11 -0500

    Atualiazando API

--- src/App.js
+++ src/App.js
@@ -3,29 +3,30 @@ import { useEffect, useState } from 'react';
 
 
 function App() {
-  const[nombre, setNombre]= useState=('');
-  const[imagen, setImgane]= useState=('');
+  const[nombre, setNombre]= useState('');
+  const[imagen, setImagen]= useState('');
 
 useEffect(()=>{
-const url= " https://randomuser.me/api/ " ;
+const url= "https://randomuser.me/api/";
 const peticion=fetch(url);
 
-  peticion
-  .then(datos=>datos.json())
-  .then(lectura=>{
-    setNombre(` ${lectura.results[0].name.first}` ` ${lectura.results[0].name.last}`)
-  })
-  .catch(()=>console.log('Mal'))
+peticion
+      .then(datos => datos.json())
+      .then(lectura => {
+        setNombre(`${lectura.results[0].name.first} ${lectura.results[0].name.last}`);
+        setImagen(lectura.results[0].picture.large);
+      })
+      .catch(() => console.log('Mal'))
 
-  },[])
 
-  
+  }, [])
 
 
   return (
     <>
       <h1>Empleado/a</h1>
       {nombre}
+ 
       <div>
         <img src={imagen} alt=""/>
       </div>
